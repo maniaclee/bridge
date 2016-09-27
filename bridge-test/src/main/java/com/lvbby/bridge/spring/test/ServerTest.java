14577e2fc226d6c073f5d3fafce2fd9a6fceaa55
peng <dianxiangan@aliyun.com>
2016-09-27 22:31:07 +0800

    web service

--- bridge-test/src/main/java/com/lvbby/bridge/spring/test/ServerTest.java
+++ bridge-test/src/main/java/com/lvbby/bridge/spring/test/ServerTest.java
@@ -3,20 +3,13 @@ package com.lvbby.bridge.spring.test;
 import com.google.common.collect.Lists;
 import com.lvbby.bridge.api.http.HttpBridgeServer;
 import com.lvbby.bridge.spring.test.service.TestServiceImpl;
-import org.junit.Test;
 
 /**
  * Created by lipeng on 16/9/26.
  */
 public class ServerTest {
 
-    @Test
-    public void run() throws Exception {
-        HttpBridgeServer httpBridgeServer = new HttpBridgeServer(Lists.newArrayList(new TestServiceImpl()));
-        httpBridgeServer.start();
-    }
-
     public static void main(String[] args) throws Exception {
-        new ServerTest().run();
+        new HttpBridgeServer(Lists.newArrayList(new TestServiceImpl())).start();
     }
 }
